6d90d0c2e9be789d4c4c24a042ed75636fc6530d
Nazire Celebi <nazcelebii@outlook.com>
2024-05-05 18:50:16 +0300

    secondCommit

--- filmDetail.js
+++ filmDetail.js
@@ -1,6 +1,8 @@
-const apiKeyDetail = '13bfb3765a40e4eb67206f9a7c9edd73';
-let filmIdDetail = (new URLSearchParams(window.location.search)).get('id');
-const filmUrlDetail = `https://api.themoviedb.org/3/movie/${filmId}?api_key=${apiKeyDetail}&language=en-US`;
+// Film detayları için API isteği
+const apiKey = '13bfb3765a40e4eb67206f9a7c9edd73';
+const urlParams = new URLSearchParams(window.location.search);
+const filmId = urlParams.get('id');
+const filmUrl = `https://api.themoviedb.org/3/movie/${filmId}?api_key=${apiKey}&language=en-US`;
 
 fetch(filmUrl)
     .then(response => response.json())
@@ -8,7 +10,7 @@ fetch(filmUrl)
         document.getElementById('filmTitle').innerText = data.title;
         document.getElementById('filmPlot').innerText = data.overview;
 
-        const videoUrl = `https://api.themoviedb.org/3/movie/${filmId}/videos?api_key=${apiKeyDetail}&language=en-US`;
+        const videoUrl = `https://api.themoviedb.org/3/movie/${filmId}/videos?api_key=${apiKey}&language=en-US`;
 
         fetch(videoUrl)
             .then(response => response.json())
@@ -17,7 +19,7 @@ fetch(filmUrl)
                 const trailerUrl = `https://www.youtube.com/embed/${trailerKey}`;
 
                 const trailerFrame = document.createElement('iframe');
-                trailerFrame.setAttribute('width', '1300');
+                trailerFrame.setAttribute('width', '70%');
                 trailerFrame.setAttribute('height', '600');
                 trailerFrame.setAttribute('src', trailerUrl);
                 trailerFrame.setAttribute('frameborder', '0');
